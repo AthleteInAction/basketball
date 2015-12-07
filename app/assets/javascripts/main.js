f09273734aaddc9510dd31313b77e28e84eeba32
Will Robinson <wrobinson-web-contractor@evernote.com>
2015-12-07 07:21:27 -0800

    Motion Error

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -7,7 +7,7 @@ $(document).on('ready page:load', function(arguments) {
 App.run(function(){
 
   var tag = document.createElement('script');
-  tag.src = "http://www.youtube.com/iframe_api";
+  tag.src = "https://www.youtube.com/iframe_api";
   var firstScriptTag = document.getElementsByTagName('script')[0];
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
